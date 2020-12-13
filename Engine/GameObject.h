1d9a75b0c76617764c0fad4ecccdefa0819a822f
FrancPS <f_xeski94@hotmail.com>
2020-12-13 12:58:20 +0100

    Hierarchy selectables

--- Engine/GameObject.h
+++ Engine/GameObject.h
@@ -12,11 +12,13 @@ class CMaterial;
 class GameObject
 {
 private:
+
 	static int objectCount;																				// GLobal counter of number of GameObjects. Used to set the uID of each new instance of GameObject.
 
 public:
 
-	char* name = "";
+	char* name = "New GameObject";
+	bool isSelected = false;
 
 private:
 
@@ -28,6 +30,7 @@ private:
 	CTransform* transform = nullptr;																	// Direct pointer to the Transform Component
 	bool hasTransform = false;																			// Indicates if this GameObject has a Transform COmponent // THIS SHOULD BE CONTROLLED IN CONSTRUCTOR and act accordignly
 
+
 public:
 		
 	GameObject();																						// Constructor
@@ -37,16 +40,17 @@ public:
 	void Update();																						// Update this GameObject with the transformations applied to it. SHOULD THIS GO IN PREUPDATE STEP?
 	void AddComponent(ComponentType _type, void* arg = nullptr, const std::string& path = "");			// Create and attach a new Component to this GameObject
 	void RemoveComponent(int _cID);																		// Detach a component from this GameObject
-	void RemoveChild(int childID);																		// Subfunction of SetParent(). Removes a GameObject (by ID) from this.children list (DOES NOT DELETE THE OBJECT)
 	void AddChild(GameObject* _newChild);																// Subfunction of SetParent(). Places another GameObject as a child of this one
-	
+	void RemoveChild(int childID);																		// Subfunction of SetParent(). Removes a GameObject (by ID) from this.children list (DOES NOT DELETE THE OBJECT)
 
 	// ---------- Getters ---------- //
-	char* GetName() { return name; }
-	CMaterial* GetMaterial() const;																		// Returns the Material Component of this GameObject, if there is one. If not, returns nullptr
-	float4x4 GetModelMatrix() const;																	// Returns the global Model Matrix defined by the Transform Components of this GameObject and its parents
-	// A getter for each kind of Component?
+	char* GetName() const { return name; }
+	const int GetUID() const { return uID; }
+	std::vector<GameObject*> GetChildren() const { return children; }
 	const bool HasTransform() const { return hasTransform; }
+	float4x4 GetModelMatrix() const;																	// Returns the global Model Matrix defined by the Transform Components of this GameObject and its parents
+	CMaterial* GetMaterial() const;																		// Returns the Material Component of this GameObject, if there is one. If not, returns nullptr
+	// A getter for each kind of Component?
 
 	// ---------- Setters ---------- //
 	void ChangeName(char* _newName) { name = _newName; }
@@ -54,5 +58,4 @@ public:
 	void SetParent(GameObject* _newParent);
 	void SetProgram(unsigned int program);
 	void SetHasTransform(bool _hasTransform) { hasTransform = _hasTransform; }
-
 };
